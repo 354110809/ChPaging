188d64c4d46fd57723e32c6de5b3a0657528d125
huichen36 <huichen36@ceditease.cn>
2016-08-23 15:41:28 +0800

    1.0.1

--- yx-paging.1.0.1.js
+++ yx-paging.1.0.1.js
@@ -4,7 +4,6 @@
 (function(w,$){
     //默认参数
     var defaults = {
-        //count : 200 总条数
         first : true,//是否开启首页
         last : true,//是否开启末页
         viewNumber : true, //是否启用每页多少条的选择功能
@@ -15,10 +14,10 @@
         viewCount : true//是否显示共多少页
     };
     var htmlReload = {
-        //html生成
+        //html初始化
         init : function(myThis){
-            this.o = $.extend(true,{},myThis.o);
-            var _o = this.o;
+            // this.o = $.extend(true,{},myThis.o);
+            var _o = myThis.o;
             var html = '';
             if(_o.first){
                 html += '<a href="javascript:;" id="yxPagingFirst">首页</a>';
@@ -48,20 +47,39 @@
             }
             myThis.$target.html(html);
             myThis.$pages = myThis.$target.find("#yxPagingBtns");
-            myThis.reloadcurrent()
+            // myThis.reloadPages();
         },
         //重新计算并渲染页标
-        reloadPages : function (options,$pages) {
+        reloadPages : function (options,$pages,$target) {
 
-            var str = '',choose = '',i,len,pageCount = options.pageCount,current = options.current;
+            var str = '',choose = '',i,len,indexLen,pageCount = options.pageCount,current = options.current;
 
-            $("#yxPagingCount").html("共" + pageCount + "页");
-            if(current >= 10){
-                i = current - 9;
+
+            $target.find("#yxPagingCount").html("共" + pageCount + "页");
+
+            if(pageCount > 10){
+                indexLen = 8;
+            }else{
+                indexLen = 10;
+            }
+
+            if(current >= 9){
+                i = current - 7;
+                if(current == pageCount ){
+                    i = current - 9;
+                    indexLen = 10
+                }else if(current == (pageCount-1) ){
+                    i = current - 8;
+                    indexLen = 9;
+                }
             }else{
                 i = 1;
             }
-            len = i + 10;
+            len = i + indexLen;
+            if(pageCount < 10){
+                len = pageCount + 1;
+            }
+
             for(; i < len; i++){
                 if(i == current){
                     choose = "class='yxPaging_choose'"
@@ -83,26 +101,113 @@
     };
     //
     var operation = {
+        //初始化
         init : function(myThis){
-            this.o = $.extend(true,{},myThis.o);
-            htmlReload.init(myThis);
+            // this.o = $.extend(true,{},myThis.o);
+            var _o = myThis.o;
+
+            if(_o.xhr){//动态分页
+                this.xhrReload(myThis);
+            }else{//静态数据
+                _o.count = _o.data.length;
+                myThis.static.start = 1;
+                myThis.static.end = _o.limit;
+
+                htmlReload.init(myThis);
+                this.reloadListCbk(myThis);
+            }
+            // this.ready(myThis);
+            //事件绑定
             this.eventBind(myThis);
+        }
+        //准备开始
+        ,operationReady : function (myThis,m) {
+            var _o = myThis.o
+                ,msg = m || {
+                    evnt : "default"
+                    ,type : "default"
+                };
+
+            //计算并保存当前开始值和结束值
+            myThis.static.end = _o.current * _o.limit;
+            myThis.static.end = (_o.count) ? (myThis.static.end <= _o.count) ? myThis.static.end : _o.count : _o.limit;
+            myThis.static.start = myThis.static.end - _o.limit + 1;
+
+
+            msg.start = myThis.static.start;
+            msg.end = myThis.static.end;
+            if(_o.count){
+                msg.count = _o.count;
+            }
+            if(_o.current){
+                msg.current = _o.current;
+            }
+            if(_o.limit){
+                msg.limit = _o.limit;
+            }
+
+            if(_o.operReady){
+                _o.operReady(msg);
+            }
+
+
+            // if(m){//操作分页
+            if(_o.xhr){
+                this.xhrReload(myThis,msg);
+            }else{
+                this.reloadListCbk(myThis,msg);
+            }
+            // }
+        }
+        //发送ajax请求
+        ,xhrReload : function (myThis,msg) {
+            var _this = this,_o = myThis.o;
+            $.ajax(_o.xhr).then(function (res) {
+                var dataObj = _o.xhrSuccess(res);//格式化数据 data,count
+                _o.data = dataObj.data;
+                _o.count = dataObj.count;
+
+                if(!msg){//判断如果是初始化则执行初始渲染
+                    htmlReload.init(myThis);
+                }
+                _this.reloadListCbk(myThis,msg)
+            });
+        }
+        //关联列表渲染
+        ,reloadListCbk : function (myThis,m) {
+            var _o = myThis.o
+                ,data;
+
+            myThis.reloadPages();
+
+
+            //如果走的静态数据则取出前页需要展示的数据
+            if(!_o.xhr){
+                data = [];
+                for(var i = myThis.static.start; i < myThis.static.end; i++){
+                    data.push(_o.data[i]);
+                }
+
+            }else{
+                data = _o.data;
+            }
+            //调用具体渲染当前页的回调函数
+            if(_o.operFinsh){
+                _o.operFinsh(data);
+            }
         }
         //事件绑定
         ,eventBind : function(myThis){
             var $myThis = $(myThis)
+                ,_this = this
                 ,_o = myThis.o
                 ,eventFn = function(e,msg){
-                    if(_o.callback){
-                        _o.callback(msg);
-                    }else{
-                        myThis.reloadPages();
-                    }
-                }
+                _this.operationReady(myThis,msg)
+            }
                 ,msg = {};
             $myThis.on("jump",eventFn).on("viewNum",eventFn);
 
-            if(this.o.viewNumber){
+            if(_o.viewNumber){
                 myThis.$target.on("change","#yxPagingViewNum",function(){//显示多少条
                     var $this = $(this),
                         val = $this.val();
@@ -116,7 +221,7 @@
                     $myThis.trigger("viewNum",[msg]);
                 });
             }
-            if(this.o.jump){
+            if(_o.jump){
                 myThis.$target.on("click","#yxPagingJumpBtn",function(){//输入页值跳转
                     var val = $("#yxPagingJumpNum").val();
                     _o.current = val || _o.current;
@@ -189,17 +294,19 @@
 
     var YxPaging = function(target,options){
         this.$target = $(target);
-        this.o = $.extend(true,defaults,options);
+        this.o = $.extend(true,{},defaults,options);
+        this.static = {};
         operation.init(this);
     };
     YxPaging.prototype = {
+
         //参数设置接口
         set : function(options){
             $.extend(true,this.o,options);
         },
         //获取参数
         get : function (str) {
-            return this.o[str];
+            return this.o[str] || this.static[str];
         },
         //重新计算多少页
         reloadPages : function(){
@@ -208,7 +315,7 @@
             if(_o.current > _o.pageCount){
                 _o.current = _o.pageCount;
             }
-            htmlReload.reloadPages(_o,this.$pages);
+            htmlReload.reloadPages(_o,this.$pages,this.$target);
         }
     };
 
